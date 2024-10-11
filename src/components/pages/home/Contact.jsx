ca02fba44eab55c01cff45cad93de66afb2343d7
Henry <viethoang9314@gmail.com>
2024-10-11 23:41:16 +1100

    Add features display, carouselto home page

--- src/components/pages/home/Contact.jsx
+++ src/components/pages/home/Contact.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from 'react';
 import { Box, TextField, Button, Typography, Grid2 as Grid } from '@mui/material';
+import SubscribeAlert from './SubscribeAlert';
 
 const ContactForm = () => {
     const [formData, setFormData] = useState({ email: '', name: '' });
+    const [subscribeAlert, setSubsribeAlert] = useState(false)
+    
+    const handleSubscribeAlertOpen = () => {
+        setSubsribeAlert(true)
+    }
 
     const handleChange = (e) => {
         const { name, value } = e.target;
@@ -11,47 +17,26 @@ const ContactForm = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        alert(`Subscribed with: ${formData.email}, Name: ${formData.name}`);
+        handleSubscribeAlertOpen()
+        // Do something here
+        // alert(`Subscribed with: ${formData.email}, Name: ${formData.name}`);
     };
 
     return (
-        <Box sx={{ textAlign: 'center', padding: '50px', backgroundColor: '#f9f9f9' }}>
-            <Typography variant="h4" gutterBottom sx={{ fontWeight: 'bold' }}>
+        <Box sx={{ textAlign: 'center', padding: '50px',  }}>
+            <Typography variant="h4" component ='h2' gutterBottom sx={{ fontWeight: 'bold' }}>
                 Get In Touch
             </Typography>
-            <Typography variant="body1" color="textSecondary" sx={{ maxWidth: '700px', margin: '0 auto', color: '#6e6e6e', fontSize: '16px' }}>
+            <Typography variant="body1" color="textSecondary" sx={{ maxWidth: '700px', margin: '0 auto', color: 'black', fontSize: '16px' }}>
                 Subscribe now to our flight price prediction service and receive personalised alerts directly to your inbox. Be the first to know when prices drop, so you can book your dream trip at the lowest possible cost.
             </Typography>
             <form onSubmit={handleSubmit} style={{ marginTop: '30px' }}>
                 <Grid container spacing={2} justifyContent="center">
-                    <Grid item size={{xs: 12, sm: 6, md: 3}}>
-                        <TextField
-                            fullWidth
-                            placeholder="Email address"
-                            type="email"
-                            name="email"
-                            variant="outlined"
-                            value={formData.email}
-                            onChange={handleChange}
-                            required
-                            InputProps={{
-                                style: { height: '40px', borderRadius: '4px', color: '#8e8e8e' },
-                            }}
-                        />
+                    <Grid item size={{xs: 12, sm: 6, md: 2.5}}>
+                        <TextField fullWidth placeholder="Email address" type="email" name="email" variant="outlined" value={formData.email} onChange={handleChange} required InputProps={{ style: { height: '40px', borderRadius: '4px', color: 'black' }, }}/>
                     </Grid>
-                    <Grid item size={{xs: 12, sm: 6, md: 3}}>
-                        <TextField
-                            fullWidth
-                            placeholder="Full name"
-                            type="text"
-                            name="name"
-                            variant="outlined"
-                            value={formData.name}
-                            onChange={handleChange}
-                            required
-                            InputProps={{
-                                style: { height: '40px', borderRadius: '4px', color: '#8e8e8e' },
-                            }}
+                    <Grid item size={{xs: 12, sm: 6, md: 2.5}}>
+                        <TextField fullWidth placeholder="Full name" type="text" name="name" variant="outlined" value={formData.name} onChange={handleChange} required InputProps={{ style: { height: '40px', borderRadius: '4px', color: 'black' }, }}
                         />
                     </Grid>
                     <Grid item size={{xs: 12, sm: 12, md: 1}}>
@@ -61,6 +46,7 @@ const ContactForm = () => {
                     </Grid>
                 </Grid>
             </form>
+            <SubscribeAlert subscribeAlert={subscribeAlert} setSubsribeAlert={setSubsribeAlert}/>
         </Box >
     );
 };
--- src/components/pages/home/HomePage.jsx
+++ src/components/pages/home/HomePage.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
-import {
-    AppBar, Toolbar, Typography, Container, Grid, Card, CardContent, Button, Box,
-    Drawer, List, ListItem, ListItemIcon, ListItemText, IconButton, TextField,
-    Switch, Snackbar, Alert, Fab, Dialog, DialogTitle, DialogContent, DialogContentText,
-    DialogActions, CircularProgress, LinearProgress, Chip, Avatar, Divider
-} from '@mui/material';
+import { Divider, Box } from '@mui/material';
 import Greeting from './Greeting';
 import Contact from './Contact';
 import Footer from '../../shared/Footer/Footer';
+import FeatureDisplay from './FeatureDisplay';
+import HomeProfileCarousel from './HomeProfileCarousel';
 
 function HomePage() {
     return (
         <Box sx={{ display: 'flex', flexDirection: 'column', minHeight: '100vh' }} >
             <Greeting />
+            {
+            // Prem add your part here 
+            }
+            <FeatureDisplay />
+            <Divider/>
+            <HomeProfileCarousel />
+            <Divider/>
             <Contact />
             <Footer />
         </Box>
